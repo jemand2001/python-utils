e2ab64708eacdd70ba7b63172b4efd1d2b63ed5d
jemand2001 <jmand@gmx.de>
2020-09-20 20:05:24 +0200

    initial commit

---
+++ utils/__init__.py
@@ -0,0 +1,16 @@
+from inspect import signature
+from functools import wraps
+
+
+def strict(f):
+    sig = signature(f)
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        for par, arg in zip(sig.parameters.values(), all_args(*args, **kwargs)):
+            if par.annotation is not par.empty and not isinstance(arg, par.annotation):
+                raise TypeError(f"parameter '{par.name}' has invalid type: expected: {par.annotation} but was: {type(arg)}")
+        res = f(*args, **kwargs)
+        if sig.return_annotation is not EMPTY and not isinstance(res, sig.return_annotation):
+            raise TypeError(f"function '{f.__name__}' returned an unexpected value: expected: {sig.return_annotation} but was: {type(res)}")
+        return res
+    return wrapper
