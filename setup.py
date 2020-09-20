0e944abd421233042249688d4a6673ebf9a1b696
jemand2001 <jmand@gmx.de>
2020-09-20 21:23:55 +0200

    added setuptools stuff, .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils',
+    version='0.0.1',
+    author='jemand2001',
+    author_email='jmand@gmx.de',
+    description='just some utilities',
+    url='https://github.com/jemand2001/utils',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8'
+)
