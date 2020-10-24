bf791a34a84d006fafb4dc03d5f13dda473bae9b
jemand2001 <jmand@gmx.de>
2020-10-24 15:54:30 +0200

    reordering

--- utils/cache.py
+++ utils/cache.py
@@ -1,20 +1,25 @@
 from enum import Enum
 from functools import wraps
 
-__all__ = ['cached', 'CacheMissException', Policy]
+__all__ = ['cached', 'CacheMissException', 'Policy']
 
 
 class CacheMissException(Exception):
     pass
 
+class Policy(Enum):
+    try_cache = 'try cache'
+    no_cache = 'no cache'
+    only_cache = 'only cache'
 
-def cached_with_policy(f):
+def cached(f):
     cache = {}
 
     @wraps(f)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, __cache_policy__=Policy.try_cache, **kwargs):
         """__cache_control__: a Policy enum member"""
-        policy = kwargs.pop("__cache_policy__", Policy.try_cache)
+        # policy = kwargs.pop("__cache_policy__", Policy.try_cache)
+        policy = __cache_policy__
         all_args = (args, tuple(kwargs.items()))
         if all_args in cache and policy is not Policy.no_cache:
             return cache[all_args]
@@ -24,9 +29,3 @@ def cached_with_policy(f):
         cache[all_args] = res
         return res
     return wrapper
-
-
-class Policy(Enum):
-    try_cache = 'try cache'
-    no_cache = 'no cache'
-    only_cache = 'only cache'
