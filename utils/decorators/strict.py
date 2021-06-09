c133f8c24c0683a94119e5b05a83db3c901b3612
jemand2001 <jmand@gmx.de>
2021-06-09 23:27:06 +0200

    made decorators.strict respect typing.Union

--- utils/decorators/strict.py
+++ utils/decorators/strict.py
@@ -2,6 +2,8 @@ from inspect import signature, Parameter
 from functools import wraps
 from typing import Any
 
+from types_.is_instance import is_instance
+
 __all__ = ['strict', 'is_empty']
 
 EMPTY = Parameter.empty
@@ -16,10 +18,10 @@ def strict(f):
         bound = sig.bind(*args, **kwargs)
         for k, v in bound.arguments.items():
             par = sig.parameters[k]
-            if not is_empty(par.annotation) and not isinstance(v, par.annotation):
+            if not is_empty(par.annotation) and not is_instance(v, par.annotation):
                 raise TypeError(f"parameter {k} has invalid type: expected: {par.annotation} but got: {type(v)}")
         res = f(*args, **kwargs)
-        if not is_empty(sig.return_annotation) and not isinstance(res, sig.return_annotation):
+        if not is_empty(sig.return_annotation) and not is_instance(res, sig.return_annotation):
             raise TypeError(f"function '{f.__name__}' returned an unexpected"
                             f" value: expected: {sig.return_annotation} but was: {type(res)}")
 
